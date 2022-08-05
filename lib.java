3d113d3c8041cf3293c7bbf7357e2d8e6c7bd88e
Anoop <anoopkuzhivayalil@gmail.com>
2022-08-05 15:25:17 +0530

    Frame created

---
+++ lib.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+class lib extends Frame {
+    
+    lib(){
+        
+        setTitle("Library");
+        setLayout(null);
+        setSize(800,600);
+        setVisible(true);
+    }
+    
+    public static void main(String args[]){
+        new lib();
+    }
+}
